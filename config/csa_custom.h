21d79af7e28143deeca8e3a7943349f093934602
Yan Pelletier <yan.pelletier.1@proton.me>
2026-08-30 21:17:22 -0400

    Adding CSA support

--- config/csa_custom.h
+++ config/csa_custom.h
@@ -5,17 +5,13 @@
 
 #pragma once
 
-#include <dt-bindings/zmk/unicode.h>
-#include "keys_fr_csa.h"
-
-#define CSA_DEG     UC(0x00B0)   // °
+#define CSA_DEG     FR_DEG       // °
 #define CSA_NEQ     UC(0x2260)   // ≠
 #define CSA_PRIME   UC(0x2032)   // ′
-#define CSA_HASH    UC(0x0023)   // #
+#define CSA_HASH    FR_HASH   // #
 #define CSA_DASH    UC(0x2013)   // –
 #define CSA_PARA    UC(0x00B6)   // ¶
 #define CSA_GRAVE   UC(0x0060)   // `
 #define CSA_PERMIL  UC(0x2030)   // ‰
 #define CSA_DPRIME  UC(0x2033)   // ″
-#define CSA_EQ      UC(0x003D)   // =
-#define CSA_PERCENT UC(0x0025)  // %
+#define CSA_EQ      FR_EQ   // =
